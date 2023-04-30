994b9e37fbfbc576eb1154ef8c429c8c35266cf9
Michael Millar <mcmelord@gmail.com>
2023-04-29 20:22:47 -0600

    update csim.c to parse cmd line args

--- csim.c
+++ csim.c
@@ -1,7 +1,103 @@
+#include <getopt.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "cachelab.h"
+
+void printHelp();
+void printError(char* msg);
+
+struct Line {
+    int valid;
+    char* tag;
+    char* data;
+};
 
 int main()
 {
-    printSummary(0, 0, 0);
+    // TODO: Parse command line arguments
+    // -h: help
+    int h = 0;
+    // -v: verbose
+    int v = 0;
+    // -s <s> number of set index bits (S = 2^s)
+    int s = -1;
+    // -E <E> number of lines per set
+    int E = -1;
+    // -b <b> number of block bits (B = 2^b)
+    int b = -1;
+    // -t <tracefile> name of trace file to replay
+    char* trace = NULL;
+
+    // Parse arguments
+    int opt;
+    while ((opt = getopt(argc, argv, "hvs:E:b:t:")) != -1) {
+        switch (opt)
+        {
+        case 'h':
+            h = 1;
+            break;
+        case 'v':
+            v = 1;
+            break;
+        case 's':
+            s = atoi(optarg);
+            break;
+        case 'E':
+            E = atoi(optarg);
+            break;
+        case 'b':
+            b = atoi(optarg);
+            break;
+        case 't':
+            trace = optarg;
+            break;
+        }
+    }
+
+    // If help set, print help
+    if (h) {
+        printHelp();
+        exit(0);
+    }
+
+    // Check all required arguments set
+    if (s == -1) {
+        printError("s is a required argument that must be set. -s <s>\n");
+        exit(1);
+    }
+    if (E == -1) {
+        printError("E is a required argument that must be set. -E <E>\n");
+        exit(1);
+    }
+    if (b == -1) {
+        printError("b is a required argument that must be set. -b <b>\n");
+        exit(1);
+    }
+    if (trace == NULL) {
+        printError("trace file is required argument that must be set. -t <trace>\n")
+        exit(1);
+    }
+
+    // TODO: Initialze data structures
+    int hit_count, miss_count, eviction_count = 0;
+
+    // TODO: Read in trace line-by-line and simulate cache
+
+    // Print Summary
+    printSummary(hit_count, miss_count, eviction_count);
     return 0;
+}
+
+void printHelp() {
+    printf("This is a cache simulator program for project 3 of UNM CS341. This program utilizes several arguments:\n")
+    printf("\t-h\t\tOptional help flag that prints usage info.\n")
+    printf("\t-v\t\tOptional verbose flag that displays trace info.\n")
+    printf("\t-s <s>\t\tNumber of set index bits (S = 2^s is number of sets)\n");
+    printf("\t-E <E>\t\tAssociativity (number of lines per set)\n");
+    printf("\t-b <b>\t\tNumber of block bits (B = 2^b is block size)\n");
+    printf("\t-t <tracefile>\t\tName of valgrind trace to replay\n");
+}
+
+void printError(char* msg) {
+    fprintf(stderr, "%s\n", msg);
 }
