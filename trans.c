e01bcec9b7634ef3ec67e4a7efe051ca20deff29
Michael Millar <mcmelord@gmail.com>
2023-05-09 19:49:02 -0600

    create tranpose for non-square presets

--- trans.c
+++ trans.c
@@ -12,6 +12,7 @@
 #include "cachelab.h"
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
+void transpose_block64(int M, int N, int A[N][M], int B[M][N]);
 void transpose_block32(int M, int N, int A[N][M], int B[M][N]);
 void transpose_block(int M, int N, int A[N][M], int B[M][N]);
 
@@ -28,7 +29,7 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N])
     if (M == 32 && N == 32) {
         transpose_block32(M, N, A, B);
     } else if (M == 64 && N == 64) {
-        transpose_block(M, N, A, B);
+        transpose_block64(M, N, A, B);
     } else {
         transpose_block(M, N, A, B);
     }
@@ -90,86 +91,72 @@ void transpose_block32(int M, int N, int A[N][M], int B[M][N])
     }
 }
 
-char transpose_block_desc[] = "A test blocking transpose";
-void transpose_block(int M, int N, int A[N][M], int B[M][N])
+char transpose_block_desc[] = "A blocking transpose for MxN Matrix";
+void transpose_block(int M, int N, int A[N][M], int B[M][N]) 
+{
+    double blockSize = 16.0;
+    int diagValue, diagIndex;
+    for (int rowBlock = 0; rowBlock < M; rowBlock+=blockSize){
+        for (int colBlock = 0; colBlock < N; colBlock+=blockSize) {
+            for (int row = colBlock; (row < N) && (row < colBlock + blockSize); row++) {
+                for (int col = rowBlock; (col < M) && (col < rowBlock + blockSize); col++) {
+                    if (row != col) {
+                        B[col][row] = A[row][col];
+                    } else {
+                        diagIndex = row;
+                        diagValue = A[diagIndex][diagIndex];
+                    }
+                }   
+                if (rowBlock == colBlock) {
+                    B[diagIndex][diagIndex] = diagValue;
+                }
+            }
+        }
+    }
+}
+
+char transpose_block64_desc[] = "A blocking transpose for 64x64 Matrix";
+void transpose_block64(int M, int N, int A[N][M], int B[M][N])
 {
     for (int rowBlock = 0; rowBlock < M; rowBlock+=4) {
         for (int colBlock = 0; colBlock < N; colBlock+=4) {
             int v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12;
 
-            // // Get first 3 rows of block
-            // v1=A[rowBlock][colBlock];
-            // v2=A[rowBlock][colBlock+1];
-            // v3=A[rowBlock][colBlock+2];
-            // v4=A[rowBlock][colBlock+3];
+            // Get first 3 rows of block
+            v1=A[rowBlock][colBlock];
+            v2=A[rowBlock][colBlock+1];
+            v3=A[rowBlock][colBlock+2];
+            v4=A[rowBlock][colBlock+3];
 
-            // v5=A[rowBlock+1][colBlock];
-            // v6=A[rowBlock+1][colBlock+1];
-            // v7=A[rowBlock+1][colBlock+2];
-            // v8=A[rowBlock+1][colBlock+3];
+            v5=A[rowBlock+1][colBlock];
+            v6=A[rowBlock+1][colBlock+1];
+            v7=A[rowBlock+1][colBlock+2];
+            v8=A[rowBlock+1][colBlock+3];
 
-            // v9=A[rowBlock+2][colBlock];
-            // v10=A[rowBlock+2][colBlock+1];
-            // v11=A[rowBlock+2][colBlock+2];
-            // v12=A[rowBlock+2][colBlock+3];
-            // // Assign B[][r+3]
-            // B[colBlock][rowBlock+3]=A[rowBlock+3][colBlock];
-            // B[colBlock+1][rowBlock+3]=A[rowBlock+3][colBlock+1];
-            // B[colBlock+2][rowBlock+3]=A[rowBlock+3][colBlock+2];
-            // B[colBlock+3][rowBlock+3]=A[rowBlock+3][colBlock+3];
-            // // Assign B[][r+2]
-            // B[colBlock][rowBlock+2]=v9;
-            // B[colBlock+1][rowBlock+2]=v10;
-            // B[colBlock+2][rowBlock+2]=v11;
-            // B[colBlock+3][rowBlock+2]=v12;
-            // // Assign B[][r+1]
-            // B[colBlock][rowBlock+1]=v5;
-            // B[colBlock+1][rowBlock+1]=v6;
-            // B[colBlock+2][rowBlock+1]=v7;
-            // B[colBlock+3][rowBlock+1]=v8;
-            // // Assign B[][r]
-            // B[colBlock][rowBlock]=v1;
-            // B[colBlock+1][rowBlock]=v2;
-            // B[colBlock+2][rowBlock]=v3;
-            // B[colBlock+3][rowBlock]=v4;
-
-            // Get last 3 columns of block
-            v1=A[rowBlock][colBlock+1];
-            v2=A[rowBlock][colBlock+2];
-            v3=A[rowBlock][colBlock+3];
-
-            v4=A[rowBlock+1][colBlock+1];
-            v5=A[rowBlock+1][colBlock+2];
-            v6=A[rowBlock+1][colBlock+3];
-
-            v7=A[rowBlock+2][colBlock+1];
-            v8=A[rowBlock+2][colBlock+2];
-            v9=A[rowBlock+2][colBlock+3];
-
-            v10=A[rowBlock+3][colBlock+1];
-            v11=A[rowBlock+3][colBlock+2];
-            v12=A[rowBlock+3][colBlock+3];
-
-            // Assign B[c][]
-            B[colBlock][rowBlock]=A[rowBlock][colBlock];
-            B[colBlock][rowBlock+1]=A[rowBlock+1][colBlock];
-            B[colBlock][rowBlock+2]=A[rowBlock+2][colBlock];
+            v9=A[rowBlock+2][colBlock];
+            v10=A[rowBlock+2][colBlock+1];
+            v11=A[rowBlock+2][colBlock+2];
+            v12=A[rowBlock+2][colBlock+3];
+            // Assign B[][r+3]
             B[colBlock][rowBlock+3]=A[rowBlock+3][colBlock];
-            // Assign B[c+1][]
-            B[colBlock+1][rowBlock]=v1;
-            B[colBlock+1][rowBlock+1]=v4;
-            B[colBlock+1][rowBlock+2]=v7;
-            B[colBlock+1][rowBlock+3]=v10;
-            // Assign B[c+2][]
-            B[colBlock+2][rowBlock]=v2;
-            B[colBlock+2][rowBlock+1]=v5;
-            B[colBlock+2][rowBlock+2]=v8;
-            B[colBlock+2][rowBlock+3]=v11;
-            // Assign B[c+3][]
-            B[colBlock+3][rowBlock]=v3;
-            B[colBlock+3][rowBlock+1]=v6;
-            B[colBlock+3][rowBlock+2]=v9;
-            B[colBlock+3][rowBlock+3]=v12;
+            B[colBlock+1][rowBlock+3]=A[rowBlock+3][colBlock+1];
+            B[colBlock+2][rowBlock+3]=A[rowBlock+3][colBlock+2];
+            B[colBlock+3][rowBlock+3]=A[rowBlock+3][colBlock+3];
+            // Assign B[][r+2]
+            B[colBlock][rowBlock+2]=v9;
+            B[colBlock+1][rowBlock+2]=v10;
+            B[colBlock+2][rowBlock+2]=v11;
+            B[colBlock+3][rowBlock+2]=v12;
+            // Assign B[][r+1]
+            B[colBlock][rowBlock+1]=v5;
+            B[colBlock+1][rowBlock+1]=v6;
+            B[colBlock+2][rowBlock+1]=v7;
+            B[colBlock+3][rowBlock+1]=v8;
+            // Assign B[][r]
+            B[colBlock][rowBlock]=v1;
+            B[colBlock+1][rowBlock]=v2;
+            B[colBlock+2][rowBlock]=v3;
+            B[colBlock+3][rowBlock]=v4;
         }   
     }
 }
